b08a49c62f545168a27f09a8b76948bea3699e2b
“Foster <fosterataylor@yahoo.com>
2020-06-16 17:36:35 -0600

    Refactor rspec

--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -7,16 +7,15 @@ Then(/^I see the text "(.*?)"$/) do |text|
 end
 
 # 1. That you are on the expected page for the Fantasy Name Generator
-Given("I am on the expected page for the Fantasy Name Generator") do
+Given("I am on the expected page") do
   find('h2', :text => "Name Generator")
 end
 
-Then("I see the current path as {string}") do |sitelink|
-  expect(page).to have_current_path(sitelink)
+Then("I see the current path as {string}") do |url|
+  expect(page).to have_current_path(url)
 end
 
-# 2. Submit the form with a specified number of names and validate that the correct number of 
-# suggestions populates
+# 2. Submit the form with a specified number of names and validate that the correct number of suggestions populates
 When("I fill in How many examples of each type would you like to generate? with {int}") do |number|
   find(:css, "input[name$='count']").set(number)
 end
@@ -30,8 +29,8 @@ Then("I see {int} name suggestions") do |numberOfSuggestions|
 end
 
 # 3. Select only one category and submit the form
-When("I select {string} button") do |uncheckAllButton|
-  click_on(uncheckAllButton)
+When("I have selected {string} button") do |uncheckAllButtonName|
+  click_on uncheckAllButtonName
 end
 
 When("I select {string} category from available category checkboxes") do |category|
@@ -48,32 +47,31 @@ end
 
 # 4. Validate that the selected category from scenario 3 is present in each entry of the list of names
 Given("I have only selected the {string} category") do |category|
-  click_on('Uncheck all')
+  click_on "Uncheck all"
   find("input[value=#{category}]").click
 end
 
-Given("I click submit") do
-  click_on(class: "create_form_submit")
+When("I click submit") do
+  click_on class: "create_form_submit"
 end
 
 Then("I see that {string} is present in each name suggestion") do |category|
   results = page.all(:css, '.name')
   results.each do |result|
-    expect(result).to have_text(category)
+    expect(result).to have_text('xyz')
   end
 end
 
-# 5. Use the "Suggest" button and validate that a human name has been added to the 
-#human name input field. Submit the form.
-Given("Human name input field is empty") do
+# 5.  Submit the form. Use the "Suggest" button and validate that a human name has been added to the human name input field.
+Given("There is no human name suggestion") do
   expect(find(:css, '.sizeMedium').value).to be_empty
 end
 
-When("I click {string} button for a human name") do |suggest|
+When("I click the {string} button for a human name") do |suggest|
   click_on suggest
 end
 
-When("I see a human name has been added") do
+Then("I see a human name has been added") do
   expect(find(:css, '.sizeMedium').value).not_to be_empty
 end
 
@@ -81,18 +79,17 @@ Then("I click submit form") do
   click_on "Write me some fantasy names"
 end
 
-# 6.Validate that the suggested human name (either first or last name) from scenario 5 
-#is present at least once in the list of names
-Given("Human name has been added") do
+# 6.Validate that the suggested human name (either first or last name) from scenario 5 is present at least once in the list of names
+Given("A human name has been suggested") do
   click_on "Suggest"
   @humanName = find(:css, '.sizeMedium').value
 end
 
-Given("Form has been submitted") do
+Given("The form has been submitted") do
   click_on(class: "create_form_submit")
 end
 
-Then("A name suggestion includes first or last human name") do
+Then("The human first or last name is included in at least one name result") do
   names = @humanName.split(' ')
   first_name = names[0]
   last_name = names[1]
