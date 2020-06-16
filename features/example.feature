af5459568f26e06f14383cf857d569c299699cf4
“Foster <fosterataylor@yahoo.com>
2020-06-16 12:48:38 -0600

    Complete initial Scenarios and Steps

--- features/example.feature
+++ features/example.feature
@@ -3,3 +3,37 @@ Feature: An Example Feature
   Scenario: An example scenario
     Given   I am on the fantasy name page
     Then    I see the text "Find the Perfect Fantasy Name"
+
+  Scenario: On the expected page
+    Given   I am on the expected page for the Fantasy Name Generator
+    Then    I see the current path as 'https://www.name-generator.org.uk/fantasy/'
+
+  Scenario: Select a number of name examples
+    When    I fill in How many examples of each type would you like to generate? with 10
+    And     I click "Write me some fantasy names" button
+    Then    I see 10 name suggestions
+
+  Scenario: Select only one name category
+    When    I select 'Uncheck all' button 
+    And     I select 'Centaur' category from available category checkboxes
+    And     I click "Write me some fantasy names" 
+    Then    I see the title 'Best Suggestions'
+
+  Scenario: Expect selected category present in every suggestion
+    Given   I have only selected the "Centaur" category 
+    Given   I click submit
+    Then    I see that "centaur" is present in each name suggestion
+
+  Scenario: Click suggest button to add human name 
+    Given   Human name input field is empty
+    When    I click "Suggest" button for a human name
+    And     I see a human name has been added
+    Then    I click submit form
+
+  Scenario: Human name is included at least once in name suggestions
+    Given   Human name has been added 
+    Given   Form has been submitted
+    Then    A name suggestion includes first or last human name
+
+
+  
--- features/step_definitions/example_steps.rb
+++ features/step_definitions/example_steps.rb
@@ -3,5 +3,101 @@ Given(/^I am on the fantasy name page$/) do
 end
 
 Then(/^I see the text "(.*?)"$/) do |text|
-  page.has_text?(text)
+  page.has_text?('xxxyyyzzz')
 end
+
+# 1. That you are on the expected page for the Fantasy Name Generator
+Given("I am on the expected page for the Fantasy Name Generator") do
+  find('h2', :text => "Name Generator")
+end
+
+Then("I see the current path as {string}") do |sitelink|
+  page.has_current_path?(sitelink)
+end
+
+# 2. Submit the form with a specified number of names and validate that the correct number of 
+# suggestions populates
+When("I fill in How many examples of each type would you like to generate? with {int}") do |number|
+  find(:css, "input[name$='count']").set(number)
+end
+
+When("I click {string} button") do |submitButtonName|
+  click_on submitButtonName
+end
+
+Then("I see {int} name suggestions") do |numberOfSuggestions|
+  page.has_css?('name', count: numberOfSuggestions)
+end
+
+# 3. Select only one category and submit the form
+When("I select {string} button") do |uncheckAllButton|
+  click_on(uncheckAllButton)
+end
+
+When("I select {string} category from available category checkboxes") do |category|
+  find("input[value=#{category}]").click
+end
+
+When("I click {string}") do |submitButtonName|
+  click_on submitButtonName
+end
+
+Then("I see the title {string}") do |suggestionsTitle|
+  page.has_text?(suggestionsTitle)
+end
+
+# 4. Validate that the selected category from scenario 3 is present in each entry of the list of names
+Given("I have only selected the {string} category") do |category|
+  click_on('Uncheck all')
+  find("input[value=#{category}]").click
+end
+
+Given("I click submit") do
+  click_on(class: "create_form_submit")
+end
+
+Then("I see that {string} is present in each name suggestion") do |category|
+  results = page.all(:css, '.name')
+  results.each do |result|
+    result.has_text?(category)
+  end
+end
+
+# 5. Use the "Suggest" button and validate that a human name has been added to the 
+#human name input field. Submit the form.
+Given("Human name input field is empty") do
+  find(:css, '.sizeMedium').value.empty?
+end
+
+When("I click {string} button for a human name") do |suggest|
+  click_on suggest
+end
+
+When("I see a human name has been added") do
+  !find(:css, '.sizeMedium').value.empty?
+end
+
+Then("I click submit form") do
+  click_on "Write me some fantasy names"
+end
+
+# 6.Validate that the suggested human name (either first or last name) from scenario 5 
+#is present at least once in the list of names
+Given("Human name has been added") do
+  click_on "Suggest"
+  @humanName = find(:css, '.sizeMedium').value
+end
+
+Given("Form has been submitted") do
+  click_on(class: "create_form_submit")
+end
+
+Then("A name suggestion includes first or last human name") do
+  names = @humanName.split(' ')
+  first_name = names[0]
+  last_name = names[1]
+  page.assert_selector('.name_heading', text: /^(#{first_name}|#{last_name})/i)
+end
+
+
+
